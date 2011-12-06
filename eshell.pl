63f422f0e12e34be5019d992916a2888d09122b0
Geoff Evans <geoff@flummoxed.org>
2011-12-06 10:55:59 -0800

    2.2.8

--- eshell.pl
+++ eshell.pl
@@ -8,7 +8,7 @@ use XML::Simple;
 use Storable qw( freeze thaw );
 use MIME::Base64;
 
-use constant VERSION => '2.2.7';
+use constant VERSION => '2.2.8';
 
 =pod
 
@@ -227,11 +227,7 @@ sub main
   #
   if( $g_EShellOptions{'runCommand'} )
   {
-    if( $g_EShellOptions{'hideConsole'} )
-    {
-      HideConsole();
-    }
-    exec( $g_EShellOptions{ 'command' } );
+    system( 1, $g_EShellOptions{ 'command' } );
   }
   elsif( $g_EShellOptions{'executeCommand'} )
   {
@@ -322,43 +318,6 @@ Usage:
   -h|-help|-usage  -- display this help text
   
 };
-}
-
-sub HideConsole()
-{
-  require Win32::API;
-
-  # just for completeness...
-  use constant SW_HIDE => 0;
-  use constant SW_SHOWNORMAL => 1;
-
-  # the API we need
-  my $GetConsoleTitle = new Win32::API('kernel32', 'GetConsoleTitle', 'PN', 'N');
-  my $SetConsoleTitle = new Win32::API('kernel32', 'SetConsoleTitle', 'P', 'N');
-  my $FindWindow = new Win32::API('user32', 'FindWindow', 'PP', 'N');
-  my $ShowWindow = new Win32::API('user32', 'ShowWindow', 'NN', 'N');
-
-  # save the current console title
-  my $old_title = " " x 1024;
-  $GetConsoleTitle->Call( $old_title, 1024 );
-
-  # build up a new (fake) title
-  my $title = "PERL-$$-" . Win32::GetTickCount();
-
-  # sets our string as the console title
-  $SetConsoleTitle->Call( $title );
-
-  # sleep 40 milliseconds to let Windows rename the window
-  Win32::Sleep(40);
-
-  # find the window by title
-  my $hw = $FindWindow->Call( 0, $title );
-
-  # restore the old title
-  $SetConsoleTitle->Call( $old_title );
-
-  # hide the console!
-  $ShowWindow->Call( $hw, SW_HIDE );
 }
 
 ################################################################################
